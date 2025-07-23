71b244e7410359d2e98b01eb3c3ce24a1a55d5fb
owwkmidream <159dreaml@gmail.com>
2025-07-24 03:51:36 +0800

    去除累赘

--- src/query.ts
+++ src/query.ts
@@ -30,26 +30,3 @@ export const getWeaponId = (queryName: string) => {
     console.error(`getWeaponrId ${queryName} 查询失败`)
     return ""
 };
-
-const elementAttrIds = [
-    {element_attr_id: 1, name: "pyro"},
-    {element_attr_id: 2, name: "anemo"},
-    {element_attr_id: 3, name: "geo"},
-    {element_attr_id: 4, name: "electro"},
-    {element_attr_id: 5, name: "hydro"},
-    {element_attr_id: 6, name: "cryo"},
-    {element_attr_id: 7, name: "dendro"},
-]
-
-
-export const getElementAttrName = (queryName: number) => {
-
-    for (let e of elementAttrIds) {
-        const {element_attr_id, name} = e
-        if (queryName == element_attr_id) {
-            return name;
-        }
-    }
-    console.error(`getElementAttrName: ${queryName} 查询失败`)
-    return ""
-};
